3c249e05836d66eccf54f28273c1d56870872c33
Nishanth-RNC <nishanthchakkravarthyr24it@psnacet.edu.in>
2026-01-22 09:45:34 +0530

    Fix Cloudinary storage format and enable permanent delete

--- server/index.js
+++ server/index.js
@@ -59,16 +59,17 @@ cloudinary.config({
 /* ---------------- APP SETUP ---------------- */
 const app = express();
 
-app.use(cors({
-  origin: [
-    'https://samkitec-frontend.onrender.com',
-    'http://localhost:5173'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://samkitec-frontend.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-app.options('*', cors());
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
